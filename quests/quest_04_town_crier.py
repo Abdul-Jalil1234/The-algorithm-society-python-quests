781b8bcae56e3f0fc012a45e05a842eab789d252
Lesliemugiraneza13 <l.mugiranez@alustudent.com>
2026-04-16 00:05:58 -0700

    Added quest_04_town_crier.py

---
+++ quests/quest_04_town_crier.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+city_name    = "Eldoria"
+current_year = 2025
+your_name    = "Alex"
+
+print(f"Welcome to {city_name}! The year is {current_year}, and our newest resident is {your_name}.")
